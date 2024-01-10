7b360056547e4229ac935e829f5f1f0c4556a13f
tudeptrai <tu416164@gmail.com>
2024-01-10 11:05:16 +0700

    create frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navigation from "./pages/Auth/Navigation";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  return (
+
+    <h1>hi
+    </h1>
+  );
+};
+
+export default App;
